fc8e723b2b7eff5b6b8b8154aa9d49a67cb1cbeb
Kellyseg <kekeliseg@gmail.com>
2023-06-21 03:21:40 -0700

    monty header file

--- monty.h
+++ monty.h
@@ -13,13 +13,13 @@
 extern char **op_toks;
 
 /**
- * struct stack_s - doubly linked list representation of a stack or queue
+ * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
- * @prev: points to the previous element of the stack or queue
- * @next: points to the next element of the stack or queue
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stacks, queues, LIFO, FIFO project
+ * for stacks, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
