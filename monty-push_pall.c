9c068ab239d762b748dda599136bb0b27d23a0d6
Kellyseg <kekeliseg@gmail.com>
2023-06-21 03:41:37 -0700

    monty push and pall

--- monty-push_pall.c
+++ monty-push_pall.c
@@ -7,8 +7,6 @@ void monty_pall(stack_t **stack, unsigned int line_number);
  * monty_push - Pushes a value to a stack_t linked list
  * @stack: A pointer to the top mode node of a stack_t linked list
  * @line_number: The current working line number of a Monty bytecodes file
- *
- * Return: nothing
  */
 void monty_push(stack_t **stack, unsigned int line_number)
 {
@@ -64,8 +62,6 @@ void monty_push(stack_t **stack, unsigned int line_number)
  * monty_pall - Prints the values of a stack_t linked list
  * @stack: A pointer to the top mode node of a stack_t linked list
  * @line_number: The current working line number of a Monty bytecodes file
- *
- * Return: nothing
  */
 void monty_pall(stack_t **stack, unsigned int line_number)
 {
